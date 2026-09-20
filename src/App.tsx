b5775f850f73a3cd662fa245b322f225a417cd75
TomMy-CL <shalynguyenshaly@gmail.com>
2026-09-21 00:30:40 +0700

    Sửa lỗi Firebase và cấu trúc Firestore

--- src/App.tsx
+++ src/App.tsx
@@ -50,6 +50,7 @@ import {
   checkinStudentToStation,
   isOrganizerAuthenticated
 } from './utils/storage';
+import {
   subscribeToStudents,
   subscribeToSingleStudent,
   saveStudentToFirestore,
